94300749085be154ab804c01329cc4cf5aad7b19
kcabhish <kcabhish@gmail.com>
2023-08-05 23:46:11 -0400

    feat: async script loading to avoid shared module error

--- products/src/index.js
+++ products/src/index.js
@@ -1,11 +1,2 @@
-import faker from 'faker';
-
-let products = '';
-
-for (let i =0; i<5 ; i++ ) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`;
-}
-
-document.querySelector('#dev-products').innerHTML = products;
-
+// doing a import in this format allows async script loading
+import('./bootstrap');
